6e9dee6ceedae8722985dc103b8e6065f6baaecc
BASANAGOUDA PATIL <47344640+basuofbgm@users.noreply.github.com>
2023-02-15 10:56:52 +1100

    Add files via upload

---
+++ NodeJSCodes/stdout.js
@@ -0,0 +1,35 @@
+// process.stdout.write("Hello  \n \n");
+
+// const questions = [
+//   "What is your name?",
+//   "What would you rather be doing?",
+//   "What is your preferred programming language"
+// ];
+// const answers = [];
+
+// function ask(i) {
+//   process.stdout.write(`\n\n\n ${questions[i]}`);
+//   process.stdout.write(` > `);
+// }
+
+// process.stdin.on("data", function (data) {
+//   process.stdout.write(data.toString().trim());
+// });
+
+// ask(answers.length);
+process.stdout.write("Hello \n\n");
+const questions =[
+  "What is your name?",
+  "What would you rather be doing?",
+  "What is preffered programming language"
+];
+const answer =[];
+function ask(i){
+  process.stdout.write(`\n\n ${questions[i]}`);
+  process.stdout.write(`>`);
+}
+
+process.stdin.on("data", function(data){
+  process.stdout.write(data.toString().trim());
+});
+ask(answer.length);
